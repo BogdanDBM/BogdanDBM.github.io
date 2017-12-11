bd4a688a1e1bfb41fd5c11df6309efd05f5fb6ef
Bogdan <bogdan.dov@gmail.com>
2017-12-12 01:24:44 +0200

    shablon stroki

---
+++ Pixelhunter/LessCode/Stroki.js
@@ -0,0 +1,6 @@
+const left = 7;
+const right = 5;
+console.log(`${`${left}*${right}`}`); //  7 * 5
+
+console.log(`${`${left}*${right}`}+${right}`); //7*5+5
+console.log(`${`${left}*${right}`}+${right} = ${left*right+right}`); //7*5+5 = 40
